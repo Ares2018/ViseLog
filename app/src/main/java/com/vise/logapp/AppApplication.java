cb36cc232216fc5f519328df21b6b8d0abcfbf59
高阳· 甄 <627312987@qq.com>
2018-06-14 14:45:31 +0800

    日志系统，添加版本管理 第一版cn.daily.android:viselog:0.0.1-SNAPSHOT

---
+++ app/src/main/java/com/vise/logapp/AppApplication.java
@@ -0,0 +1,27 @@
+package com.vise.logapp;
+
+import android.app.Application;
+
+import com.vise.log.ViseLog;
+import com.vise.log.inner.ConsoleTree;
+import com.vise.log.inner.FileTree;
+import com.vise.log.inner.LogcatTree;
+
+/**
+ * @Description:
+ * @author: <a href="http://www.xiaoyaoyou1212.com">DAWI</a>
+ * @date: 16/12/11 20:13.
+ */
+public class AppApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if(BuildConfig.DEBUG){
+            ViseLog.getLogConfig().configAllowLog(true)
+                    .configShowBorders(true);
+            ViseLog.plant(new FileTree(this, "Log"));
+            ViseLog.plant(new ConsoleTree());
+            ViseLog.plant(new LogcatTree());
+        }
+    }
+}
